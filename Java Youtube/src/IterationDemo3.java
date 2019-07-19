e8a00189a6a4557f27c31fa47d79ec075442001c
navee <navee@DESKTOP-DSL3P0A.lan>
2019-07-19 09:29:53 +0200

    First commit, upload iterationdemo3.java case

---
+++ Java Youtube/src/IterationDemo3.java
@@ -0,0 +1,36 @@
+
+//* * * *
+
+public class IterationDemo3
+{
+
+	public static void main(String[] args)
+	
+	{
+		int i=1;
+		{
+              System.out.print(i + " ");
+              
+			}
+			System.out.println();  // NEW LINE ONLY
+			
+		for(int j=1;j<=2;j++)
+			{
+	              System.out.print(j + " "); 
+			}
+	              System.out.println();  // NEW LINE ONLY
+	              
+	    for(int k=1;k<=3;k++)
+	  			{
+	  	              System.out.print(k + " "); 		           
+	  			}
+	    System.out.println();  // NEW LINE ONLY
+	    
+	    for(int l=1;l<=4;l++)
+			{
+	              System.out.print(l + " "); 		           
+			}
+          System.out.println();  // NEW LINE ONLY
+	    
+	    }
+	}
